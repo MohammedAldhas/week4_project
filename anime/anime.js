69ac4fd12f89f593a625bda42bdc08882436de39
Mohammed <mohammed.aldhas3@gmail.com>
2023-08-18 21:42:00 +0300

    4

--- anime/anime.js
+++ anime/anime.js
@@ -6,28 +6,51 @@ hint.addEventListener("mouseout", function () {
   hint.innerHTML = `<i class="fa-solid fa-face-smile-wink me-2" style="color: #000000;"></i>تلميح`;
 });
 
-hint.addEventListener("click",function(){
-    document.querySelector(".err").innerText = "اول شكل بريك"
-    document.querySelector(".err").style.color = "green"
-})
+hint.addEventListener("click", function () {
+    if(window.location.toString().includes("page1")){
 
-let answer = document.querySelector(".answer")
-let answerButton = document.querySelector(".next")
+        document.querySelector(".err").innerText = "التلمح ان هذا الانمي عمك";
+        document.querySelector(".err").style.color = "green";
+    }else if(window.location.toString().includes("page2")){
 
-
-
-answer.onfocus = function(){
-    document.querySelector(".err").innerText = "حاول تفصل كل صورة وتجمعها مع بعض"
-    document.querySelector(".err").style.color = "black"
-}
-
-answerButton.addEventListener("click",function(e){
-    if(answer.value == "بريكينج باد" || answer.value == "بريكينق باد"){
-        answer.value = ""
-    }else{
-        document.querySelector(".err").innerText = "غلط عليك حاول مره ثانية"
-        document.querySelector(".err").style.color = "red"
-        e.preventDefault()
+        document.querySelector(".err").innerText = "موت بالانجليزي ايش؟";
+        document.querySelector(".err").style.color = "green";
     }
+});
 
-})
+let answerPage1 = document.querySelector(".answer_page1");
+let answerPage2 = document.querySelector(".answer_page2");
+let answerPage3 = document.querySelector(".answer_page3");
+let answerButton = document.querySelector(".next");
+
+// answer.onfocus = function () {
+//     document.querySelector(".err").innerText =
+//       "حاول تفصل كل صورة وتجمعها مع بعض";
+//     document.querySelector(".err").style.color = "black";
+
+// };
+
+answerButton.addEventListener("click", function (e) {
+  if (answerPage1.value == "ون بيس" || answerPage1.value == "ونبيس") {
+    answerPage1.value = "";
+    window.location="./anime_easy_page2.html"
+  } else {
+    document.querySelector(".err").innerText = "غلط عليك حاول مره ثانية";
+    document.querySelector(".err").style.color = "red";
+    e.preventDefault();
+  }
+});
+answerButton.addEventListener("click", function (e) {
+  if (answerPage2.value == "ديث نوت" || answerPage2.value == "ديثنوت") {
+    answerPage2.value = "";
+    window.location="./anime_easy_page3.html"
+  } else {
+    document.querySelector(".err").innerText = "غلط عليك حاول مره ثانية";
+    document.querySelector(".err").style.color = "red";
+    e.preventDefault();
+  }
+});
+
+setTimeout(() => {
+  document.querySelector(".left-right").innerHTML = `<i class="fa-solid fa-face-laugh-squint me-2"></i>هذي سهلة وما جبتها باقي`;
+}, "5000");
