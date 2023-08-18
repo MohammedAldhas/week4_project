03ce952d40915998ad21cb1078b4f5e9992b67a7
Mohammed <mohammed.aldhas3@gmail.com>
2023-08-18 23:00:02 +0300

    animeEsy

--- anime/anime.js
+++ anime/anime.js
@@ -13,8 +13,15 @@ hint.addEventListener("click", function () {
   } else if (window.location.toString().includes("page2")) {
     document.querySelector(".err").innerText = "موت بالانجليزي ايش؟";
     document.querySelector(".err").style.color = "green";
-  }else if (window.location.toString().includes("page3")) {
-    document.querySelector(".err").innerText = "ما احس تحتاج تميح بس معليه التلميح هو اكثر انمي فيه صراخ";
+  } else if (window.location.toString().includes("page3")) {
+    document.querySelector(".err").innerText =
+      "ما احس تحتاج تلميح بس معليه التلميح هو اكثر انمي فيه صراخ";
+    document.querySelector(".err").style.color = "green";
+  } else if (window.location.toString().includes("page4")) {
+    document.querySelector(".err").innerText = "اللي في النص بنش";
+    document.querySelector(".err").style.color = "green";
+  }else if (window.location.toString().includes("page5")) {
+    document.querySelector(".err").innerText = "اخر سؤال يا هوه مافي مشكلة طابور كيو";
     document.querySelector(".err").style.color = "green";
   }
 });
@@ -22,6 +29,8 @@ hint.addEventListener("click", function () {
 let answerPage1 = document.querySelector(".answer_page1");
 let answerPage2 = document.querySelector(".answer_page2");
 let answerPage3 = document.querySelector(".answer_page3");
+let answerPage4 = document.querySelector(".answer_page4");
+let answerPage5 = document.querySelector(".answer_page5");
 let answerButton = document.querySelector(".next");
 
 // answer.onfocus = function () {
@@ -69,7 +78,39 @@ answerButton.addEventListener("click", function (e) {
     e.preventDefault();
   }
 });
+//one punch man
+answerButton.addEventListener("click", function (e) {
+  if (
+    answerPage4.value == "ون بنش مان" ||
+    answerPage4.value == "ون بانش مان" ||
+    answerPage4.value == "وان بانش مان" ||
+    answerPage4.value == "ون بنتش مان" ||
+    answerPage4.value == "وانبانشمان" ||
+    answerPage4.value == "ونبانشمان"
+  ) {
+    answerPage4.value = "";
+    window.location = "./anime_easy_page5.html";
+  } else {
+    document.querySelector(".err").innerText = "غلط عليك حاول مره ثانية";
+    document.querySelector(".err").style.color = "red";
+    e.preventDefault();
+  }
+});
 
+answerButton.addEventListener("click", function (e) {
+  if (
+    answerPage5.value == "هايكيو" ||
+    answerPage5.value == "هيكيو" ||
+    answerPage5.value == "هاي كيو"
+  ) {
+    answerPage5.value = "";
+    window.location = "../end.html";
+  } else {
+    document.querySelector(".err").innerText = "غلط عليك حاول مره ثانية";
+    document.querySelector(".err").style.color = "red";
+    e.preventDefault();
+  }
+});
 setTimeout(() => {
   document.querySelector(
     ".left-right"
